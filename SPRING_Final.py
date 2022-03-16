1216fc057fc3c8b5252a1ea12aa97a66477583b2
Ericdajib <xc1044220200@gmail.com>
2022-03-16 10:03:54 +0100

    创建power类,实时plot Leistung

--- SPRING_Final.py
+++ SPRING_Final.py
@@ -6,7 +6,6 @@ from scipy import signal
 import heapq as hq
 from numba import cuda,jit
 
-import matplotlib.pyplot as plt
 
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
@@ -118,13 +117,29 @@ class Signal():
         self.getMatchedSequence()
 
 
-class Power(Signal):
+class Metric():
 
-    def __init__(self):
+    def __init__(self,signal1,signal2):
+        self.presentSequenceLength = len(signal1.presentSequence)
+        self.presentSequence = np.zeros(self.presentSequenceLength)
 
 
-    def updateSequence(self,inputSignal1,inputSignal2):
-        self.st = inputSignal1*inputSignal2
+    def updateSequence(self,signal1,signal2):
+        self.st = signal1.st * signal2.st
+        self.presentSequence[1:] = self.presentSequence[:-1]
+        self.presentSequence[0] = self.st
+
+
+    def setPlot1(self,title = None,pen = "white"):
+        self.p1 = win.addPlot(title=title)
+        self.curve1 = self.p1.plot(self.presentSequence[::-1], pen=pen)
+
+    def updatePlot1(self):
+        global N
+        self.curve1.setData(self.presentSequence[::-1])
+        self.curve1.setPos(N,0)
+
+
 
 
 
@@ -133,18 +148,13 @@ currentWithCmt = Signal(fullSequencePath="V2BCurrent_Segment.csv", querySequence
 CurrentWithPuls = Signal(fullSequencePath="V2BCurrent_Segment.csv", querySequencePath="V2BCurrent_Puls.csv", threshold= 10000)
 voltageWithZuendfehler = Signal(fullSequencePath="V2BVoltage_Segment.csv", querySequencePath="V2BVoltage_Zuendfehler.csv", threshold= 600)
 voltageWithSpritzer = Signal(fullSequencePath="V2BVoltage_Segment.csv", querySequencePath="V2BVoltage_Spritzer5.csv", threshold= 200)
-
-
-
-
-
+power = Metric(currentWithCmt,voltageWithZuendfehler)
 
 
 app = pg.mkQApp("Spring Dashboard")
 win = pg.GraphicsLayoutWidget(show=True)
 win.setWindowTitle("Spring basic Dashboard")
 win.resize(1000,600)
-
 
 currentWithCmt.setPlot1("Strom Datastream",pen=(217,83,25))
 currentWithCmt.setPlot2("CMT")
@@ -156,8 +166,13 @@ voltageWithZuendfehler.setPlot1("Spannung Datastream",pen=(0,114,189))
 voltageWithZuendfehler.setPlot2("Zündfehler")
 voltageWithSpritzer.setPlot2("Spritzer")
 
+win.nextRow()
+
+power.setPlot1("Leistung Datastream",pen=(126,47,142))
+
 qGraphicsGridLayout = win.ci.layout
 qGraphicsGridLayout.setColumnStretchFactor(0,2)
+
 
 
 def updateData():
@@ -166,6 +181,7 @@ def updateData():
     CurrentWithPuls.updateData()
     voltageWithZuendfehler.updateData()
     voltageWithSpritzer.updateData()
+    power.updateSequence(currentWithCmt,voltageWithZuendfehler)
 
     currentWithCmt.updatePlot1()
     currentWithCmt.updatePlot2()
@@ -174,6 +190,9 @@ def updateData():
     voltageWithZuendfehler.updatePlot1()
     voltageWithZuendfehler.updatePlot2()
     voltageWithSpritzer.updatePlot2()
+
+    power.updatePlot1()
+
     N+=1
 
 
